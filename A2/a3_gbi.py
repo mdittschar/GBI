554b992e7e07114403f3755cd98d437a98a415b6
mdittschar <marina@dittschar.de>
2022-05-09 18:18:25 +0200

    A2 matrix initialisation + gap penalties

--- A2/a3_gbi.py
+++ A2/a3_gbi.py
@@ -1,11 +1,40 @@
 import Bio
 from Bio import SeqIO
+import getopt
+import sys
+import numpy as np
+import pandas as pd
+
+def get_seqs():
+    ses = [None] * (len(sys.argv)-1)
+    for i, val in enumerate(sys.argv[1:]):
+        for record in SeqIO.parse(val, "fasta"):
+            se = record.seq
+
+            ses[i] = se
+   
+    return ses
+
+def compute(sequences, match, mismatch, gap):
+    rows = len(sequences[0]) + 1
+    columns = len(sequences[1]) + 1
+    str_index = list(str(sequences[0]))
+    str_index.insert(0,'0')
+    str_columns = list(str(sequences[1]))
+    str_columns.insert(0, '0')
+    matrix = pd.DataFrame(columns = str_columns, index=str_index)
+    print(matrix)
+
+    matrix.loc["0"] = np.arange(len(str_columns)) * gap
+    matrix.loc[:,"0"] = np.arange(len(str_index)) * gap
+    print(matrix)
 
 
-
-for record in SeqIO.parse(r"material/unknown_protein.fasta", "fasta"):
-    print(record.seq)
-
+if __name__ == "__main__":
+    
+    sequences = get_seqs()
+    print("Length of sequence 1: ",len(sequences[0]))
+    compute(sequences, -2, 2, 4)
 
 # Needleman-Wunsch
 
