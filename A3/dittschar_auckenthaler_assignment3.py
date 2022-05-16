0f47ca084155d8d77da7fd42c710a4d5425ea38d
mdittschar <marina@dittschar.de>
2022-05-16 17:45:13 +0200

    Task 3 until cross combination scores

--- A3/dittschar_auckenthaler_assignment3.py
+++ A3/dittschar_auckenthaler_assignment3.py
@@ -30,7 +30,7 @@ def get_args():
     # get the command line arguments
     argv = sys.argv[1:]
     # get a tuple of the arguments to use
-    opts, _ = getopt.getopt(argv, "a:bcdm:s:g:", ['file1', 'file2', 'file3', 'file4', 'match', 'mismatch', 'gap'])
+    opts, _ = getopt.getopt(argv, "a:b:cdm:s:g:", ['file1', 'file2', 'file3', 'file4', 'match', 'mismatch', 'gap'])
     ses = [None] * 4
 
     ids = []
@@ -106,7 +106,9 @@ def compute(sequences, match, mismatch, gap):
         for row in np.arange(1,rows): 
             # get either match or mismatch score
             # since there is an additional zero-row and zero-column, reference is minus one
-            if str_seq0[row- 1] == str_seq1[column- 1]:
+            if str_seq0[row- 1] == "-" and str_seq1[column- 1] == "-": 
+                vmatch = 0
+            elif str_seq0[row- 1] == str_seq1[column- 1]:
                 vmatch = S[row-1, column-1] + match
             else: 
                 vmatch = S[row-1, column-1] + mismatch
@@ -191,6 +193,82 @@ def traceback(S, T, rows, columns, sequence0, sequence1):
             # add gap character to second sequence
             tstring0 = tstring0 + str_seq0[cur_row]
             tstring1 = tstring1 + "-"
+
+    # reverse the completed alignment strings
+    tstring0 = tstring0[::-1]
+    tstring1 = tstring1[::-1]
+
+    # get the optimal alignment score
+    opt_score = S[rows-1, columns-1]
+    return opt_score, match_no, mismatch_no, gap_no, tstring0, tstring1
+
+
+def traceback_cross(S, T, rows, columns, sequence0, sequence1):
+    """
+    Trace back the optimal alignment for a given sequence and traceback matrix
+
+    Parameters:
+    -----------
+        S (np.array): sequence matrix with alignment scores
+        T (np.array): traceback matrix with directions
+        rows (int): number of rows in S and T
+        columns (int): number of columns in S and T
+        sequence0 (Seq): Sequence 1 to align
+        sequence1 (Seq): Sequence 2 to align
+
+    Returns:
+    --------
+        opt_score (int): optimal global alignment score
+        match_no (int): number of matches
+        mismatch_no(int): number of mismatches 
+        gap_no (int): number of gaps 
+        tstring0 (str): string 1 of optimal alignment
+        tstring1 (str): string 2 of optimal alignments
+    """
+
+    # traceback
+    # get a characterwise list of the sequences
+    str_seq0 = list(str(sequence0))
+    str_seq1 = list(str(sequence1))
+    # initialise current row and column
+    cur_row = rows - 1
+    cur_col = columns - 1
+    # initialise alignment strings
+    tstring0 = ""
+    tstring1 = ""
+    # initialise match, mismatch and gap number
+    match_no = 0
+    mismatch_no = 0
+    gap_no = 0
+    # while we have not gone through the whole matrix
+    while cur_col != 0 and cur_row != 0:
+        if T[cur_row, cur_col] == "diagonal":
+            # if characters match, increase match number
+            if str_seq0[cur_row -1] == str_seq1[cur_col - 1]:
+                match_no = match_no + 1
+            # else, increase mismatch number
+            else:
+                mismatch_no = mismatch_no + 1
+            # go back up diagonally
+            cur_row = cur_row - 1
+            cur_col = cur_col - 1
+            tstring0 = tstring0 + str_seq0[cur_row]
+            tstring1 = tstring1 + str_seq1[cur_col]
+        elif T[cur_row, cur_col] == "left":
+            # go one cell to the left
+            cur_col = cur_col - 1
+            gap_no = gap_no + 1
+            # add gap character to first sequence
+            tstring0 = tstring0 + "x"
+            tstring1 = tstring1 + str_seq1[cur_col]
+        else:
+            # go one cell up
+            cur_row = cur_row - 1 
+            gap_no = gap_no + 1
+            # add gap character to second sequence
+            tstring0 = tstring0 + str_seq0[cur_row]
+            tstring1 = tstring1 + "x"
+
     # reverse the completed alignment strings
     tstring0 = tstring0[::-1]
     tstring1 = tstring1[::-1]
@@ -261,16 +339,87 @@ def visual_alignment(tstring0, tstring1,filename, ids, match_no, mismatch_no, ga
         file_out.close
       
 
-def main():
-     
-    sequences,ids, match, mismatch, gap= get_args()
+def get_multiple_alignments(sequences,ids, match, mismatch, gap):
+        
     # get matrices and number of rows/columns
+    
+    opt_scores = []
+    list_strings0 = []
+    list_strings1 = []
     for comb in list(map(list,combinations([0,1,2,3],2))):
         seqs_consider = itemgetter(comb[0], comb[1])(sequences)
         S, T, rows, columns =  compute(seqs_consider, match, mismatch, gap)
         opt_score, match_no, mismatch_no, gap_no, astring0, astring1 = traceback(S, T, rows, columns, seqs_consider[0], seqs_consider[1])
-        visual_alignment(astring0, astring1, "dittschar_auckenthaler_assignment2_global_alignment.txt", ids, match_no, mismatch_no, gap_no, match, mismatch, gap)
+        list_strings0.append(astring0)
+        list_strings1.append(astring1)
+        opt_scores = np.append(opt_scores, opt_score)
+        
+    combs = list(map(list,combinations([0,1,2,3],2)))
+    print("opt_scores = ", opt_scores)
+    print("List strings 0: ", list_strings0)
 
+    max_score_arg = np.argmax(opt_scores)
+    A_max = [list_strings0[max_score_arg], list_strings1[max_score_arg]]
+    print("A_max: ", A_max)
+    #a_rest_inds = [x for x in [0,1,2,3] if x not in combs[max_score_arg]]
+    rest_ind = [i for i, x in enumerate(combs) if x[0] not in combs[max_score_arg] and x[1] not in combs[max_score_arg]][0]
+
+    A_rest = [list_strings0[rest_ind], list_strings1[rest_ind]]
+    print("A_rest: ", A_rest)
+    
+    for i, max_seq in enumerate(A_max):
+        for j, rest_seq in enumerate(A_rest):
+            S, T, rows, columns =  compute([max_seq, rest_seq], match, mismatch, gap)
+            opt_score, match_no, mismatch_no, gap_no, astring0, astring1 = traceback_cross(S, T, rows, columns, max_seq, rest_seq)
+            #print("Astring 0: ", astring0)
+        
+            print("Optimal score for this combination: ", opt_score)
+
+    
+
+    # combs_reduced = [x for x in combs if x not in [combs[max_score_arg], comb_rest_inds]]
+    # inds_reduced = [i for i, x in enumerate(combs) if x not in [combs[max_score_arg], comb_rest_inds]]
+    # opt_scores_reduced = [opt_scores[i] for i, x in enumerate(combs) if x not in [combs[max_score_arg], a_rest_inds]]
+
+    
+    #max_cross = np.argmax(opt_scores_reduced)
+    #print("Max cross score: ", max_cross)
+   # A_cross = [list_strings0[inds_reduced[max_cross]], list_strings1[inds_reduced[max_cross]]]
+    #print("A_cross: ", A_cross)
+
+
+    #visual_alignment(astring0, astring1, "dittschar_auckenthaler_assignment2_global_alignment.txt", ids, match_no, mismatch_no, gap_no, match, mismatch, gap)
+
+def main():
+     
+    sequences,ids, match, mismatch, gap= get_args()
+    # get matrices and number of rows/columns
+    get_multiple_alignments(sequences,ids, match, mismatch, gap)
+
+    #opt_scores = []
+    # for comb in list(map(list,combinations([0,1,2,3],2))):
+    #     seqs_consider = itemgetter(comb[0], comb[1])(sequences)
+    #     S, T, rows, columns =  compute(seqs_consider, match, mismatch, gap)
+    #     opt_score, match_no, mismatch_no, gap_no, astring0, astring1 = traceback(S, T, rows, columns, seqs_consider[0], seqs_consider[1])
+    #     visual_alignment(astring0, astring1, "dittschar_auckenthaler_assignment2_global_alignment.txt", ids, match_no, mismatch_no, gap_no, match, mismatch, gap)
+    #     opt_scores = np.append(opt_scores, opt_score)
+        
+    # combs = list(map(list,combinations([0,1,2,3],2)))
+    # print("combinations: ", list(map(list,combinations([0,1,2,3],2))))
+    # print("opt_scores = ", opt_scores)
+
+    # max_score_arg = np.argmax(opt_scores)
+    # A_max = [sequences[combs[max_score_arg][0]], sequences[combs[max_score_arg][1]]]
+    
+    # a_rest_inds = [x for x in [0,1,2,3] if x not in combs[max_score_arg]]
+    # print("A rest inds: ", a_rest_inds)
+    # A_rest = [sequences[a_rest_inds[0]], sequences[a_rest_inds[1]]]
+    # combs_reduced = [x for x in combs if x not in [combs[max_score_arg], a_rest_inds]]
+    # opt_scores_reduced = [opt_scores[i] for i, x in enumerate(combs) if x not in [combs[max_score_arg], a_rest_inds]]
+    # print("Combs reduced: ", combs_reduced)
+    # max_cross = np.argmax(opt_scores_reduced)
+    # print("Max cross score: ", max_cross)
+    #A_cross = 
     
     #S, T, rows, columns =  compute(sequences, match, mismatch, gap)
     # get optimal alignment score as well as number of matches, mismatches and gaps and aligned strings
