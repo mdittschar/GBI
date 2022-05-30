f0e159c9101a9dea2b81637aa71b00769f52fa8c
mdittschar <marina@dittschar.de>
2022-05-30 17:10:56 +0200

    Put file names in output and clean up

--- A5/dittschar_auckenthaler_assignment5.py
+++ A5/dittschar_auckenthaler_assignment5.py
@@ -37,11 +37,8 @@ def get_mat(arg):
                         first_val = line[1]
                         line_vals = [float(first_val)] + [0]*(9-idx)
                     else: 
-                        line_vals = [float(i) for i in line[1:]] + [0]*(9-idx)
-                    print("Name: ",line[0])                             
+                        line_vals = [float(i) for i in line[1:]] + [0]*(9-idx)                           
                     names.append(line[0])
-                    
-                    print(line_vals)
                     values.append(np.array(line_vals))
         return names, values
         
@@ -50,28 +47,30 @@ def open_dist():
     opts, _ = getopt.getopt(argv, "a:b:c:", ['file1', 'file2', 'file3'])
     names_list = []
     values_list = []
+    mat_names = []
     for opt, arg in opts:
         if opt in ["-a", "--file1", "-b", "--file2", "-c", "--file3"]:
             print(f"File1: {arg}")
             names, values = get_mat(arg)
             names_list.append(names)
             values_list.append(values)
+            mat_names.append(arg)
             
     
     dist_df_1 = pd.DataFrame(values_list[0], index=names_list[0])
     dist_df_2 = pd.DataFrame(values_list[1], index=names_list[1])
     dist_df_3 = pd.DataFrame(values_list[2], index=names_list[2])
     
-    return dist_df_1, dist_df_2, dist_df_3
+    return dist_df_1, dist_df_2, dist_df_3, mat_names
 
 
 
 def main():
-    orig_dist_df, dist_df_1, dist_df_2 = open_dist()
-    print(dist_df_2)
+    orig_dist_df, dist_df_1, dist_df_2, mat_names= open_dist()
     c1 = ccc(orig_dists = orig_dist_df, new_dists=dist_df_1)
     c2 = ccc(orig_dists = orig_dist_df, new_dists=dist_df_2)
-    print(f"C1 is: {c1}\nC2 is {c2}")
+    print(f"CCC between {mat_names[0]} and {mat_names[1]} is: {c1}")
+    print(f"CCC between {mat_names[0]} and {mat_names[2]} is: {c2}")
 
 
 
