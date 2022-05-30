c1dfefc5218de87c91e97dfa2a76ce9061e4ef27
mdittschar <marina@dittschar.de>
2022-05-30 16:38:35 +0200

    take the mean and index at location

--- A5/dittschar_auckenthaler_assignment5.py
+++ A5/dittschar_auckenthaler_assignment5.py
@@ -3,35 +3,59 @@ import sys
 import csv
 import pandas as pd
 import numpy as np
+import itertools
 
 def ccc(orig_dists, new_dists):
+    orig_mean = np.mean(np.asarray(orig_dists).astype(float))
+    new_mean = np.mean(np.asarray(new_dists).astype(float))
+    for i, val in enumerate(orig_dists.index):
+        for j in np.arange(i+1):
+            print("idx:",orig_dists.loc[orig_dists.index[i], j])
     return
 
-def get_df(arg):
+def get_mat(arg):
         values = []
         names = []
         with open(arg) as file_in:
-            for line in csv.reader(file_in, delimiter="\t"):    
-                if len(line) > 1:                                  
+            for idx, line in enumerate(csv.reader(file_in, delimiter="\t")):
+                if idx >0:
+                    if idx == 1:
+                        first_val = line[1]
+                        line_vals = [float(first_val)] + [0]*(9-idx)
+                    else: 
+                        line_vals = line[1:] + [0]*(9-idx)
+                    print("Name: ",line[0])                             
                     names.append(line[0])
-                    values.append(line[1:])
-        dist_df = pd.DataFrame(values, index=names, columns=names)
+                    
+                    print(line_vals)
+                    values.append(np.array(line_vals))
+        return names, values
+        
 def open_dist():
     argv = sys.argv[1:]
     opts, _ = getopt.getopt(argv, "a:b:c:", ['file1', 'file2', 'file3'])
+    names_list = []
+    values_list = []
     for opt, arg in opts:
         if opt in ["-a", "--file1", "-b", "--file2", "-c", "--file3"]:
             print(f"File1: {arg}")
-            dist_df = get_df(arg)
-
+            names, values = get_mat(arg)
+            names_list.append(names)
+            values_list.append(values)
             
-    return dist_df
+    
+    dist_df_1 = pd.DataFrame(values_list[0], index=names_list[0])#, columns=names_list[0])
+    dist_df_2 = pd.DataFrame(values_list[1], index=names_list[1])#, columns=names_list[1])
+    dist_df_3 = pd.DataFrame(values_list[2], index=names_list[2])#, columns=names_list[2])
+    
+    return dist_df_1, dist_df_2, dist_df_3
 
 
 
 def main():
-    open_dist()
-    #ccc(orig_dists, new_dists)
+    dist_df_1, dist_df_2, dist_df_3 = open_dist()
+    print(dist_df_2)
+    ccc(orig_dists = dist_df_1, new_dists=dist_df_2)
 
 
 
