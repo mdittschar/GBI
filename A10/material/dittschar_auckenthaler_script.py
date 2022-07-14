73d1c230871a6c9a49a8c90d7a9e33d0ba05ddcb
mdittschar <marina@dittschar.de>
2022-07-14 10:37:48 +0200

    Consider both strands

--- A10/material/dittschar_auckenthaler_script.py
+++ A10/material/dittschar_auckenthaler_script.py
@@ -15,12 +15,12 @@ def specificity(tn, fp):
 
 
 input_file = sys.argv[1:]
-opts, _ = getopt.getopt(input_file, "a:b:r:", ['input1', 'input2', 'reference'])
+opts, _ = getopt.getopt(input_file, "a:b:r:", ['prokka', 'genemark', 'reference'])
 for opt, arg in opts:
-    if opt in ["-a", "--input1"]:
+    if opt in ["-a", "--prokka"]:
         with open(arg) as f:
             lines = f.readlines()
-    elif opt in ["-b", "--input2"]:
+    elif opt in ["-b", "--genemark"]:
         with open(arg) as f:
             lines2 = f.readlines()
     elif opt in ["-r", "--reference"]:
@@ -31,31 +31,47 @@ out = myGffParser(lines)
 out2 = myGffParser(lines2)
 ref = myGffParser(reference)
 
-pred_arr1 = np.zeros((6264404))
+pred_arr1_neg = np.zeros((6264404))
+pred_arr1_pos = np.zeros((6264404))
 for i in out.df.index:
-    pred_arr1[out.df.loc[i, "start"]:out.df.loc[i, "end"]] = 1
+    if out.df.loc[i, "type"] == "CDS":
+        if out.df.loc[i, "strand"] == "-":
+            pred_arr1_neg[out.df.loc[i, "start"]:out.df.loc[i, "end"]] = 1
+        elif out.df.loc[i, "strand"] == "+":
+            pred_arr1_pos[out.df.loc[i, "start"]:out.df.loc[i, "end"]] = 1
 
-pred_arr2 = np.zeros((6264404))
+
+pred_arr2_neg = np.zeros((6264404))
+pred_arr2_pos = np.zeros((6264404))
 for i in out2.df.index:
-    pred_arr2[out2.df["start"].loc[i]:out2.df["end"].loc[i]] = 1
+    if out2.df.loc[i, "type"] == "CDS":
+        if out2.df.loc[i, "strand"] == "-":
+            pred_arr2_neg[out2.df.loc[i, "start"]:out2.df.loc[i, "end"]] = 1
+        elif out2.df.loc[i, "strand"] == "+":
+            pred_arr2_pos[out2.df.loc[i, "start"]:out2.df.loc[i, "end"]] = 1
 
-ref_arr = np.zeros((6264404))
+
+ref_arr_neg = np.zeros((6264404))
+ref_arr_pos = np.zeros((6264404))
 for i in ref.df.index:
     if ref.df.loc[i, "type"] == "CDS":
-        ref_arr[ref.df.loc[i, "start"]:ref.df.loc[i, "end"]] = 1
+        if ref.df.loc[i, "strand"] == "-":
+            ref_arr_neg[ref.df.loc[i, "start"]:ref.df.loc[i, "end"]] = 1
+        elif ref.df.loc[i, "strand"] == "+":
+            ref_arr_pos[ref.df.loc[i, "start"]:ref.df.loc[i, "end"]] = 1
 
 
 
-tp_1 = np.sum(np.logical_and(pred_arr1 == 1, ref_arr == 1))#/np.sum(ref_arr)
+tp_1 = np.sum(np.logical_and(pred_arr1_neg == 1, ref_arr_neg == 1)) + np.sum(np.logical_and(pred_arr1_pos == 1, ref_arr_pos == 1))#/np.sum(ref_arr)
 print("True positive rate of PROKKA: ", tp_1)
 
-tn_1 = np.sum(np.logical_and(pred_arr1 == 0, ref_arr == 0))#/(len(ref_arr) - np.sum(ref_arr))
+tn_1 = np.sum(np.logical_and(pred_arr1_neg == 0, ref_arr_neg == 0)) + np.sum(np.logical_and(pred_arr1_pos == 0, ref_arr_pos == 0))#/(len(ref_arr) - np.sum(ref_arr))
 print("True negative rate of PROKKA: ", tn_1)
 
-fp_1 = np.sum(np.logical_and(pred_arr1 == 1, ref_arr == 0))#/(len(ref_arr) - np.sum(ref_arr))
+fp_1 = np.sum(np.logical_and(pred_arr1_neg == 1, ref_arr_neg == 0)) + np.sum(np.logical_and(pred_arr1_pos == 1, ref_arr_pos == 0))#/(len(ref_arr) - np.sum(ref_arr))
 print("False positive rate of PROKKA: ", fp_1)#
 
-fn_1 = np.sum(np.logical_and(pred_arr1 == 0, ref_arr == 1))#/np.sum(ref_arr)
+fn_1 = np.sum(np.logical_and(pred_arr1_neg == 0, ref_arr_neg == 1)) + np.sum(np.logical_and(pred_arr1_pos == 0, ref_arr_pos == 1))#/np.sum(ref_arr)
 print("False negative rate of PROKKA: ", fn_1)
 
 acc_1 = acc(tp_1, tn_1, fp_1, fn_1)
@@ -70,15 +86,15 @@ print(f"Total length is: {tp_1+tn_1+fp_1+fn_1}")
 print()
 
 # values for Genemark output
-tp_2 = np.sum(np.logical_and(pred_arr2 == 1, ref_arr == 1))#/np.sum(ref_arr)
+tp_2 = np.sum(np.logical_and(pred_arr2_neg == 1, ref_arr_neg == 1)) + np.sum(np.logical_and(pred_arr2_pos == 1, ref_arr_pos == 1))#/np.sum(ref_arr)
 print("True positive rate of Genemark: ", tp_2)
 
-tn_2 = np.sum(np.logical_and(pred_arr2 == 0, ref_arr == 0))#/(len(ref_arr) - np.sum(ref_arr))
+tn_2 = np.sum(np.logical_and(pred_arr2_neg == 0, ref_arr_neg == 0)) + np.sum(np.logical_and(pred_arr2_pos == 0, ref_arr_pos == 0))#/(len(ref_arr) - np.sum(ref_arr))
 print("True negative rate of Genemark: ", tn_2)
 
-fp_2 = np.sum(np.logical_and(pred_arr2 == 1, ref_arr == 0))#/(len(ref_arr) - np.sum(ref_arr))
+fp_2 = np.sum(np.logical_and(pred_arr2_neg == 1, ref_arr_neg == 0)) + np.sum(np.logical_and(pred_arr2_pos == 1, ref_arr_pos == 0))#/(len(ref_arr) - np.sum(ref_arr))
 print("False positive rate of Genemark: ", fp_2)#
-fn_2 = np.sum(np.logical_and(pred_arr2 == 0, ref_arr == 1))#/np.sum(ref_arr)
+fn_2 = np.sum(np.logical_and(pred_arr2_neg == 0, ref_arr_neg == 1))+np.sum( np.logical_and(pred_arr2_pos == 0, ref_arr_pos == 1))#/np.sum(ref_arr)
 print("False negative rate of Genemark: ", fn_2)
 
 acc_2 = acc(tp_2, tn_2, fp_2, fn_2)
